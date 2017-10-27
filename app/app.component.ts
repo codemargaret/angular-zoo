c7ec2e62e4a24e3dcf00117c43b5175a682281e8
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-10-27 09:09:04 -0700

    create animal class

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Zoo Animal Tracker</h1>
+
+  </div>
+    `
+  })
+
+export class AppComponent {
+
+}

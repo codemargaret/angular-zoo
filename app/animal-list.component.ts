a9dcf4e9bc0be1dce433069a6e506abd9197f7e8
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-10-27 09:42:32 -0700

    add animal list component

---
+++ app/animal-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+    <ul>
+      <li *ngFor="let currentAnimal of childAnimalList"> Name: {{currentAnimal.name}}
+      <ul>
+        <li>Species: {{currentAnimal.species}}</li>
+        <li>Age: {{currentAnimal.age}}</li>
+        <li>Diet: {{currentAnimal.diet}}</li>
+        <li>Location: {{currentAnimal.location}}</li>
+        <li>Caretakers: {{currentAnimal.caretakers}}</li>
+        <li>Sex: {{currentAnimal.sex}}</li>
+        <li>Likes: {{currentAnimal.likes}}</li>
+        <li>Dislikes: {{currentAnimal.dislikes}}</li>
+      </ul>
+      </li>
+    </ul>
+  `
+})
+
+export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,11 +6,17 @@ import { Animal } from './animal.model';
   template: `
   <div class="container">
     <h1>Zoo Animal Tracker</h1>
-
+      <animal-list [childAnimalList]="masterAnimalList"></animal-list>
   </div>
     `
   })
 
 export class AppComponent {
+  masterAnimalList: Animal[] =  [
+    new Animal('Polar Bear', 'Cuddles', 5, 'carnivore', 'Bear Pit', 2, 'male', 'long walks on an iceberg', 'vegetables'),
+    new Animal('Green Tree Frog', 'Fluffy', 1, 'omnivore', 'South American Building', 2, 'female', 'flies', 'French people'),
+    new Animal('African Grey Parrot', 'Alexa', 1, 'herbivore', 'Bird Cage', 16, 'female', 'intellectual conversations', 'crackers')
+  ];
 
+  selectedAnimal = null;
 }
